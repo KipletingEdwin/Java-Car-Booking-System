bf7502709b8cb9eaee0e21dd2174a5c1d2d91680
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-02-05 11:40:01 +0000

    Add getters to the BookedCarInformation

--- src/BookedCarInformation.java
+++ src/BookedCarInformation.java
@@ -1,2 +1,25 @@
 public class BookedCarInformation {
+
+    private  Customer customer;
+    private  Car car;
+    private  int days;
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public int getDays() {
+        return days;
+    }
+
+    public BookedCarInformation(Customer customer, Car car, int days) {
+        this.customer = customer;
+        this.car = car;
+        this.days = days;
+    }
+
 }
