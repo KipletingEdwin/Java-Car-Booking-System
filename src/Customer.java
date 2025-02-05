4b5a594b1a7a374d4a97fbd0942582d97096badc
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-02-05 10:11:51 +0000

    Add two variables (name and id ) to the customer class. It also includes the constructor

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,11 @@
 public class Customer {
+
+    private  String name;
+    private  String id;
+
+    // Constructor
+    public Customer(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
 }
