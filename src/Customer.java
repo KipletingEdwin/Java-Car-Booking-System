a323dbd585322b4664182ebb89e022efe2e95240
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-02-05 10:12:46 +0000

    Add getters and setters to the customer class

--- src/Customer.java
+++ src/Customer.java
@@ -3,6 +3,22 @@ public class Customer {
     private  String name;
     private  String id;
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
     // Constructor
     public Customer(String name, String id) {
         this.name = name;
